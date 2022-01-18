2cbb69cfad93e0d668415783ae66fdc5feb6c1e6
mmccarthy64 <mmccarthy64@hotmail.com>
2022-01-18 14:17:42 -0600

    finished

--- pickrandomword.py
+++ pickrandomword.py
@@ -2,35 +2,49 @@ import random
 from words import words
 import string
 
-print(words)
+# print(words)
 
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
-    return word
+    return word.upper()
 
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word)
+    word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()
+    used_letters = set() #keep track of what letters have been guessed
 
-    while len(word_letters) > 0:
-        print('You have used these letters: ', ' '.join(used_letters))
+    lives = 7
 
+    while len(word_letters) > 0 and lives > 0:
+        #letters used
+        print('You have', lives, 'lives. You have used these letters: ', ' '.join(used_letters))
+
+        #current W - R D progress
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
+
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
+            else:
+                lives = lives - 1
+                print('Letter not in word.')
+
         elif user_letter in used_letters:
             print('You have already guessed that character. Please try again.')
         else:
             print('You did not use a real character, try again.')
 
-user_input = input('Type something: ')
-print(user_input())
+            #gets here when word letters == 0 or lives == 0
+    if lives == 0:
+        print('You have died. The word was ', word)
+    else:
+        print('You guessed the word ', word, '!!!')
+
+hangman()
